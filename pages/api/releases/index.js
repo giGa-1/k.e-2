6a9e64e483dd8d884e656c2ec89a78bcc62f4241
BuildTools <unconfigured@null.spigotmc.org>
2023-03-10 23:18:45 +0200

    add month

--- pages/api/releases/index.js
+++ pages/api/releases/index.js
@@ -4,22 +4,24 @@ const monthNames = ["January", "February", "March", "April", "May", "June",
   "July", "August", "September", "October", "November", "December"
 ];
 
-var cacheDate = new Date();
-var cache = null;
+var cacheDate = [];
+var cache = [];
 
 export default function handle(req, res) {
     return new Promise((resolve, reject) => {
         var date = new Date();
 
+        var month = req.query.month;
+
         //10 minute cache
-        if(cache != null && date.valueOf() - cacheDate.valueOf() < 600 * 1000) {
-            res.status(200).json(cache);
+        if(cache[month] != null && date.valueOf() - cacheDate[month].valueOf() < 600 * 1000) {
+            res.status(200).json(cache[month]);
             resolve();
         } else {
-            getJson(`v2.1/films/releases?year=${date.getFullYear()}&month=${monthNames[date.getMonth()].toUpperCase()}`).then((json) => {
+            getJson(`v2.1/films/releases?year=${date.getFullYear()}&month=${monthNames[month].toUpperCase()}`).then((json) => {
                 res.status(200).json(json);
-                cache = json;
-                cacheDate = date;
+                cache[month] = json;
+                cacheDate[month] = date;
                 resolve();
             });
         }
