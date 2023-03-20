e1f4e36f46dd30250ab103a01c3878ce3d9ddc74
BuildTools <unconfigured@null.spigotmc.org>
2023-03-20 21:09:15 +0200

    handle fetch errors?

--- internal/api-relay.js
+++ internal/api-relay.js
@@ -5,5 +5,7 @@ export default function getJson(path) {
 			'X-API-KEY': '8cf31ece-1bd7-4c03-824e-7613feddfdb3',
 			'Content-Type': 'application/json',
 		},
+	}).catch(err => {
+		console.log(`fetch err ${err}`)
 	}).then(res => res.json());
 }
