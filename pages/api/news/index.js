d40e0d009827ab26df0623a1bcd4e1f9c80f3566
BuildTools <unconfigured@null.spigotmc.org>
2023-03-20 21:34:56 +0200

    fix

--- pages/api/news/index.js
+++ pages/api/news/index.js
@@ -23,23 +23,27 @@ export default function handle(req, res) {
         fetch(`https://kg-portal.ru/news/movies/${num}/`, {
             method: 'GET',
         }).catch(err => {
-            res.status(200).json([]);
-            resolve();
-            return;
-        }).then(resp => resp.text()).then(text => {
-            const { document } = (new JSDOM(text)).window;
-            let collection = document.getElementsByClassName("news_box movies_cat");
-            let promises = [];
-            for (let i = 0; i < collection.length; i++) {
-                promises[i] = getData(collection.item(i).getElementsByTagName("a")[0].href);
-            }
-            Promise.all(promises).then((values) => {
-                cache.set(num, {date: new Date(), value: values});
-                
-                console.log("fetched news from web");
-                res.status(200).json(values);
+        }).then(resp => {
+            if(resp != undefined) {
+                resp.text().then(text => {
+                    const { document } = (new JSDOM(text)).window;
+                    let collection = document.getElementsByClassName("news_box movies_cat");
+                    let promises = [];
+                    for (let i = 0; i < collection.length; i++) {
+                        promises[i] = getData(collection.item(i).getElementsByTagName("a")[0].href);
+                    }
+                    Promise.all(promises).then((values) => {
+                        cache.set(num, {date: new Date(), value: values});
+                        
+                        console.log("fetched news from web");
+                        res.status(200).json(values);
+                        resolve();
+                    });
+                })
+            } else {
+                res.status(200).json([]);
                 resolve();
-            });
+            }
         });
     });
 }
@@ -49,32 +53,36 @@ function getData(link) {
         fetch(`https://kg-portal.ru${link}`, {
             method: 'GET',
         }).catch(err => {
-            res({});
-            return;
-        }).then(resp => resp.text()).then(text => {
-            const { document } = (new JSDOM(text)).window;
-            try {
-                let title = document.getElementsByClassName("news_title").item(0).textContent;
-                let date = document.getElementsByClassName("date").item(0).attributes.getNamedItem("content").nodeValue;
-
-                let div = document.getElementsByClassName("news_text").item(0);
-
-                let pic = "";
-                for (let i = 0; i < div.children.length; i++) {
-                    let element = div.children.item(i);
-                    if(element.className == "news_cover_center") {
-                        pic = "https://kg-portal.ru" + element.children.item(0).getElementsByTagName("img").item(0).src;
+        }).then(resp => {
+            if(resp != undefined) {
+                resp.text().then(text => {
+                    const { document } = (new JSDOM(text)).window;
+                    try {
+                        let title = document.getElementsByClassName("news_title").item(0).textContent;
+                        let date = document.getElementsByClassName("date").item(0).attributes.getNamedItem("content").nodeValue;
+        
+                        let div = document.getElementsByClassName("news_text").item(0);
+        
+                        let pic = "";
+                        for (let i = 0; i < div.children.length; i++) {
+                            let element = div.children.item(i);
+                            if(element.className == "news_cover_center") {
+                                pic = "https://kg-portal.ru" + element.children.item(0).getElementsByTagName("img").item(0).src;
+                            }
+                        }
+        
+                        let content = "";
+                        let arr = div.getElementsByTagName("p");
+                        for (let i = 0; i < arr.length; i++) {
+                            content += arr.item(i).textContent;
+                        }
+        
+                        res({title: title, text: content, coverUrl: pic, date: date});
+                    } catch (ex) {
+                        res({});
                     }
-                }
-
-                let content = "";
-                let arr = div.getElementsByTagName("p");
-                for (let i = 0; i < arr.length; i++) {
-                    content += arr.item(i).textContent;
-                }
-
-                res({title: title, text: content, coverUrl: pic, date: date});
-            } catch (ex) {
+                })
+            } else {
                 res({});
             }
         });
