aacaa992d01550b403b56f1f1996e47ebb032f37
Shmeb <1gledpobgornuk@gmail.com>
2023-03-20 20:51:34 +0300

    fix news and loader

--- src/components/MediaPage/MediaAllComp.jsx
+++ src/components/MediaPage/MediaAllComp.jsx
@@ -30,8 +30,8 @@ export default function MediaAllComp() {
     useEffect(()=>{
         const response = getNewsAPIjs('page=1');
         const response2 = getNewsAPIjs('page=2');
-        const response3 = getNewsAPIjs('page=3');
-        const response4 = getNewsAPIjs('page=4');
+        // const response3 = getNewsAPIjs('page=3');
+        // const response4 = getNewsAPIjs('page=4');
 
 
         
@@ -41,41 +41,60 @@ export default function MediaAllComp() {
         })
         response2.then((data)=>{
             setStateNewsN([...stateNewsN, ...data].filter((e)=>e.coverUrl!==''))
+            setIsLoading(true)
         })
-        response3.then((data)=>{
-            setStateArticles([...stateArticles, ...data])
-        }) 
-        response4.then((data)=>{
-            setStateArticlesN([...stateArticlesN, ...data])
-        })
+        // response3.then((data)=>{
+        //     setStateArticles([...stateArticles, ...data])
+        // }) 
+        // response4.then((data)=>{
+        //     setStateArticlesN([...stateArticlesN, ...data])
+        // })
         const getStateTops = getTopsAPI();
         getStateTops.then((data)=>{
             dispatch(setStateTops(data))
+            setStateArticles(data)
         })
        
 
     },[])
-    useMemo(()=>{
-        if (stateNews.length) {
-            console.log(stateNews)
-            setIsLoading(true)
-        }
-    },[stateNews])
+ 
 
     const [stateArrNews, setStateArrNews] = useState(0);
 
   return (
     <div className={cl.allComp}>
+          <div className={!isLoading ? ["loaderBLock", "loaderActive"].join` ` :"loaderBLock"}>
+            <div className={"loader"}>
+                <span className={"loadSvg"}>
+                <svg version="1.1" id="loader-1"  x="0px" y="0px"
+                    width="100px" height="100px" viewBox="0 0 40 40" enableBackground="new 0 0 40 40" >
+                    <path opacity="0.2" fill="#fff" d="M20.201,5.169c-8.254,0-14.946,6.692-14.946,14.946c0,8.255,6.692,14.946,14.946,14.946
+                        s14.946-6.691,14.946-14.946C35.146,11.861,28.455,5.169,20.201,5.169z M20.201,31.749c-6.425,0-11.634-5.208-11.634-11.634
+                        c0-6.425,5.209-11.634,11.634-11.634c6.425,0,11.633,5.209,11.633,11.634C31.834,26.541,26.626,31.749,20.201,31.749z"/>
+                    <path fill="#fff" d="M26.013,10.047l1.654-2.866c-2.198-1.272-4.743-2.012-7.466-2.012h0v3.312h0
+                        C22.32,8.481,24.301,9.057,26.013,10.047z">
+                        <animateTransform attributeType="xml"
+                        attributeName="transform"
+                        type="rotate"
+                        from="0 20 20"
+                        to="360 20 20"
+                        dur="0.5s"
+                        repeatCount="indefinite"/>
+                        </path>
+                    </svg>
+                </span>
+            </div>
+        </div>
         <div className={cl.newsAllBlock}>
             <MyTitleComp classTitle={cl.tabsTitle} isCenter={false} >Новости кино</MyTitleComp>
             <div className={cl.newsAllListBlock}>
                 <ul className={cl.newsAllList}>
                     {
-                        isLoading&&[...stateNews, ...stateNewsN].filter((e,i)=>i<=17).map((e,i)=>{
+                        isLoading&&[...stateNews, ...stateNewsN].filter(e=>Object.keys(e).length !== 0).filter((e,i)=>i<=17).map((e,i)=>{
                             return (
                                 <React.Fragment key={i}>
                                    
-                                  <MediaNewsItem descr={e.title} index={i} img={e.coverUrl}/>
+                                  <MediaNewsItem descr={e.title} index={i} href={'/news/'+e.date} img={e.coverUrl}/>
                                    
                                 </React.Fragment>
                             )
@@ -102,7 +121,7 @@ export default function MediaAllComp() {
             <MyTitleComp classTitle={cl.tabsTitle} isCenter={false} >Интересные статьи для вас</MyTitleComp>
             <div className={cl.articlesAllListBlock}>
                 <ul className={cl.articlesAllList}>
-                    {[...stateArticlesN, ...statetops].filter((e,i)=>i<=24).map((e,i)=>{
+                    {[...statetops, ...stateArticles].filter((e,i)=>i<=24).map((e,i)=>{
                         return (
                             <React.Fragment key={i}>
                                 <Link href={'/event/'+e.id} className={cl.allLinkArticles}>
--- src/components/MediaPage/MediaArticlesComp.jsx
+++ src/components/MediaPage/MediaArticlesComp.jsx
@@ -46,9 +46,21 @@ export default function MediaArticlesComp() {
                 </ul>
             </div>
               <div className={cl.pagination}>
-            <span className={cl.arrowCircleLeft} onClick={event=>changePageVideos(event, 0)}></span>
+            <span className={cl.arrowCircleLeft} onClick={event=>changePageVideos(event, 0)}>
+              <svg width="25px" height="25px" viewBox="0 0 24 24" fill="none">
+              <path d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke="#f1f1f1" strokeWidth="2"/>
+              <path d="M8 12L16 12" stroke="#f1f1f1" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+              <path d="M11 9L8.08704 11.913V11.913C8.03897 11.961 8.03897 12.039 8.08704 12.087V12.087L11 15" stroke="#f1f1f1" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+              </svg>
+            </span>
             <span className={cl.countPage}>{isPage}</span>
-            <span className={cl.arrowCircleRight} onClick={event=>changePageVideos(event, 1)}></span>
+            <span className={cl.arrowCircleRight} onClick={event=>changePageVideos(event, 1)}>
+            <svg width="25px" height="25px" viewBox="0 0 24 24" fill="none">
+              <path d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke="#f1f1f1" strokeWidth="2"/>
+              <path d="M8 12L16 12" stroke="#f1f1f1" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+              <path d="M11 9L8.08704 11.913V11.913C8.03897 11.961 8.03897 12.039 8.08704 12.087V12.087L11 15" stroke="#f1f1f1" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+              </svg>
+            </span>
         </div>
         </div>
   )
