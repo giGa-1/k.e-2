2138d131ba7ab9a06f5e227da2d41dd9c088623e
BuildTools <unconfigured@null.spigotmc.org>
2023-03-26 15:53:31 +0300

    return article and photos in movie info

--- pages/api/movie/[id].js
+++ pages/api/movie/[id].js
@@ -1,5 +1,18 @@
 import { getStatments, selectMovieStmt, insertMovieStmt } from "../../../internal/database";
 import getJson from "../../../internal/api-relay";
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+function getArticle(name) {
+	return fetch(`https://ru.wikipedia.org/w/api.php?action=query&titles=${name}&formatversion=2&format=json&prop=extracts&explaintext=1&redirects`, {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	}).catch(err => {
+		console.log(`fetch err ${err}`)
+	}).then(res => res.json());
+}
 
 export default function handler(req, res) {
 	return new Promise((resolve, reject) => {
@@ -15,28 +28,80 @@ export default function handler(req, res) {
 							getJson(`v2.2/films/${id}`), 
 							getJson(`v2.2/films/${id}/box_office`), 
 							getJson(`v2.2/films/${id}/distributions`), 
-							getJson(`v1/staff?filmId=${id}`)
+							getJson(`v1/staff?filmId=${id}`),
 						]).then((values) => {
 							let json = values[0];
 							let boxJson = values[1];
 							let viewJson = values[2];
 							let staffJson = values[3];
+
 							let jsonData = {json, boxJson, viewJson, staffJson};
 		
 							insertMovieStmt.run(id, JSON.stringify(jsonData));
 		
 							console.log(`Data for movie id ${id} retrieved from API!`);
-							res.status(200).json(jsonData);
-							resolve();
+
+							Promise.all([
+								fetch(`https://www.imdb.com/title/${jsonData.json.imdbId}/mediaindex/?ref_=tt_mi_sm`).then(resp => resp.text()),
+								getArticle(jsonData.json.nameOriginal)
+							]).then(values => {
+								const { document } = (new JSDOM(values[0])).window;
+								if(document.getElementById("media_index_thumbnail_grid") != undefined) {
+									let tags = document.getElementById("media_index_thumbnail_grid").getElementsByTagName("a");
+									let imgs = [];
+									for (let i = 0; i < tags.length; i++) {
+										imgs.push(tags.item(i).firstChild.src.split("_V1_")[0] + "_V1_.jpg");
+									}
+			
+									jsonData.photos = imgs;	
+								} else {
+									jsonData.photos = [];
+								}
+								
+		
+								let wiki = values[1];
+								if(wiki.query.pages.length > 0 && !wiki.query.pages[0].missing) {
+									jsonData.article = wiki.query.pages[0].extract;
+								} else {
+									jsonData.article = "";
+								}
+		
+								res.status(200).json(jsonData);
+								resolve();
+							});
 						});
 					} catch(ex) {
 						res.status(500);
 					}
 				} else {
-					console.log(`Data for movie id ${id} retrieved locally!`);
+					let respData = JSON.parse(responce.json);
+					Promise.all([
+						fetch(`https://www.imdb.com/title/${respData.json.imdbId}/mediaindex/?ref_=tt_mi_sm`).then(resp => resp.text()),
+						getArticle(respData.json.nameOriginal)
+					]).then(values => {
+						const { document } = (new JSDOM(values[0])).window;
+						if(document.getElementById("media_index_thumbnail_grid") != undefined) {
+							let tags = document.getElementById("media_index_thumbnail_grid").getElementsByTagName("a");
+							let imgs = [];
+							for (let i = 0; i < tags.length; i++) {
+								imgs.push(tags.item(i).firstChild.src.split("_V1_")[0] + "_V1_.jpg");
+							}
+	
+							respData.photos = imgs;	
+						} else {
+							respData.photos = [];
+						}
 
-					res.status(200).json(JSON.parse(responce.json));
-					resolve();
+						let wiki = values[1];
+						if(wiki.query.pages.length > 0 && !wiki.query.pages[0].missing) {
+							respData.article = wiki.query.pages[0].extract;
+						} else {
+							respData.article = "";
+						}
+
+						res.status(200).json(respData);
+						resolve();
+					});
 				}
 			});
 		});
